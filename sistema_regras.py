56bbbb190ec9f8759d8670b20b6df74e5642ff90
Vinícius <vinitonii@gmail.com>
2024-09-24 21:18:53 -0300

    fixes 3.0

--- sistema_regras.py
+++ sistema_regras.py
@@ -6,9 +6,9 @@ class SistemaRegras:
         self.semaforo_atual = None  #Semáforo atualmente aberto
 
     def aplicar_regras(self, fila_veiculos):
-        #Se não houver um semáforo aberto ou ele já passou o tempo mínimo aberto
+
         if self.semaforo_atual is None or self.semaforo_atual.tempo_aberto >= 10:
-            
+
             #Seleciona o semáforo com a maior fila
             direcao_maior_fila = max(fila_veiculos, key=lambda direcao: len(fila_veiculos[direcao]))
             semaforo_prioritario = next((s for s in self.semaforos if s.direcao == direcao_maior_fila), None)
@@ -17,9 +17,8 @@ class SistemaRegras:
             if semaforo_prioritario and semaforo_prioritario != self.semaforo_atual:
                 self.abrir_semaforo(semaforo_prioritario)
 
-        #Atualizar os tempos e fechar o semáforo atual após o tempo de abertura mínimo (10 segundos)
         for semaforo in self.semaforos:
-            #Fechar o semáforo atual se ele já ficou aberto por tempo suficiente
+            #Fechar o semáforo atual ficou aberto pelo tempo estimado (10 segundos)
             if semaforo == self.semaforo_atual and semaforo.tempo_aberto >= 10:
                 semaforo.fechar()
                 self.semaforo_atual = None  #Libera para abrir outro semáforo
@@ -28,7 +27,7 @@ class SistemaRegras:
             semaforo.atualizar_tempo()
 
     def abrir_semaforo(self, semaforo):
-        #Fecha todos os outros semáforos antes de abrir o novo prioritário
+        #Fecha todos os outros semáforos antes de abrir o próximo prioritário
         for s in self.semaforos:
             if s != semaforo:
                 s.fechar()
